6ffc233faaee39840875d514c4d0143b5ed32f71
mario <mario@digitalstate.ca>
2018-02-20 15:18:38 -0500

    Add the concept of roles (refactor to the latest core security component, update fixtures and migrations)

--- src/AppBundle/Migrations/Version1_0_0.php
+++ src/AppBundle/Migrations/Version1_0_0.php
@@ -32,11 +32,10 @@ class Version1_0_0 extends AbstractMigration
         $this->addSql('CREATE TABLE ds_config (id INT NOT NULL, uuid UUID NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, "key" VARCHAR(255) NOT NULL, "value" TEXT DEFAULT NULL, enabled BOOLEAN NOT NULL, version INT DEFAULT 1 NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_758C45F4D17F50A6 ON ds_config (uuid)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_758C45F4F48571EB ON ds_config ("key")');
-        $this->addSql('CREATE TABLE ds_access (id INT NOT NULL, uuid UUID NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, identity VARCHAR(255) DEFAULT NULL, identity_uuid UUID DEFAULT NULL, version INT DEFAULT 1 NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE ds_access (id INT NOT NULL, uuid UUID NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, assignee VARCHAR(255) DEFAULT NULL, assignee_uuid UUID DEFAULT NULL, version INT DEFAULT 1 NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_A76F41DCD17F50A6 ON ds_access (uuid)');
-        $this->addSql('CREATE TABLE ds_access_permission (id INT NOT NULL, access_id INT DEFAULT NULL, entity VARCHAR(255) DEFAULT NULL, entity_uuid UUID DEFAULT NULL, "key" VARCHAR(255) NOT NULL, attributes JSON NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE ds_access_permission (id INT NOT NULL, access_id INT DEFAULT NULL, scope VARCHAR(255) DEFAULT NULL, entity VARCHAR(255) DEFAULT NULL, entity_uuid UUID DEFAULT NULL, "key" VARCHAR(255) NOT NULL, attributes JSON NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_D46DD4D04FEA67CF ON ds_access_permission (access_id)');
-        $this->addSql('CREATE TABLE ds_session (id VARCHAR(128) NOT NULL PRIMARY KEY, data BYTEA NOT NULL, time INTEGER NOT NULL, lifetime INTEGER NOT NULL)');
         $this->addSql('CREATE TABLE app_case (id INT NOT NULL, uuid UUID NOT NULL, custom_id VARCHAR(255) NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, identity VARCHAR(255) DEFAULT NULL, identity_uuid UUID DEFAULT NULL, state VARCHAR(255) DEFAULT NULL, priority INT DEFAULT 0 NOT NULL, version INT DEFAULT 1 NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_7D26BCA4D17F50A6 ON app_case (uuid)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_7D26BCA4614A603A ON app_case (custom_id)');
@@ -53,6 +52,8 @@ class Version1_0_0 extends AbstractMigration
         $this->addSql('ALTER TABLE app_case_status ADD CONSTRAINT FK_8B0B0409CF10D4F5 FOREIGN KEY (case_id) REFERENCES app_case (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE app_case_status_trans ADD CONSTRAINT FK_12F776492C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES app_case_status (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE app_case_trans ADD CONSTRAINT FK_74D34F2D2C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES app_case (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        $this->addSql('CREATE TABLE ds_session (id VARCHAR(128) NOT NULL PRIMARY KEY, data BYTEA NOT NULL, time INTEGER NOT NULL, lifetime INTEGER NOT NULL)');
 
         // Data
         $yml = file_get_contents('/srv/api-platform/src/AppBundle/Resources/migrations/1_0_0.yml');
@@ -81,81 +82,22 @@ class Version1_0_0 extends AbstractMigration
 
         $this->addSql('
             INSERT INTO 
-                ds_access (id, uuid, owner, owner_uuid, identity, identity_uuid, version, created_at, updated_at)
+                ds_access (id, uuid, owner, owner_uuid, assignee, assignee_uuid, version, created_at, updated_at)
             VALUES 
                 (1, \''.Uuid::uuid4()->toString().'\', \'System\', \''.$data['identity']['system']['uuid'].'\', \'System\', \''.$data['identity']['system']['uuid'].'\', 1, now(), now()),
-                (2, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'Anonymous\', NULL, 1, now(), now()),
-                (3, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'Individual\', NULL, 1, now(), now()),
-                (4, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'Organization\', NULL, 1, now(), now()),
-                (5, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'Staff\', NULL, 1, now(), now()),
-                (6, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'Staff\', \''.$data['identity']['admin']['uuid'].'\', 1, now(), now());
+                (2, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'Staff\', \''.$data['identity']['admin']['uuid'].'\', 1, now(), now());
         ');
 
         $this->addSql('
             INSERT INTO 
-                ds_access_permission (id, access_id, entity, entity_uuid, key, attributes)
+                ds_access_permission (id, access_id, scope, entity, entity_uuid, key, attributes)
             VALUES 
-                (1, 1, \'BusinessUnit\', NULL, \'entity\', \'["BROWSE","READ","EDIT","ADD","DELETE"]\'),
-                (2, 1, \'BusinessUnit\', NULL, \'property\', \'["BROWSE","READ","EDIT"]\'),
-                (3, 1, \'BusinessUnit\', NULL, \'custom\', \'["BROWSE","READ","EDIT","ADD","DELETE","EXECUTE"]\'),
-                (4, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case\', \'["BROWSE","READ"]\'),
-                (5, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_uuid\', \'["BROWSE","READ"]\'),
-                (6, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_custom_id\', \'["BROWSE","READ"]\'),
-                (7, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_created_at\', \'["BROWSE","READ"]\'),
-                (8, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_title\', \'["BROWSE","READ"]\'),
-                (9, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_data\', \'["BROWSE","READ"]\'),
-                (10, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_state\', \'["BROWSE","READ"]\'),
-                (11, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_statuses\', \'["BROWSE","READ"]\'),
-                (12, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_version\', \'["BROWSE","READ"]\'),
-                (13, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status\', \'["BROWSE","READ"]\'),
-                (14, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_uuid\', \'["BROWSE","READ"]\'),
-                (15, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_created_at\', \'["BROWSE","READ"]\'),
-                (16, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_case\', \'["BROWSE","READ"]\'),
-                (17, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_title\', \'["BROWSE","READ"]\'),
-                (18, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_description\', \'["BROWSE","READ"]\'),
-                (19, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_data\', \'["BROWSE","READ"]\'),
-                (20, 2, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_version\', \'["BROWSE","READ"]\'),
-                (21, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case\', \'["BROWSE","READ"]\'),
-                (22, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_uuid\', \'["BROWSE","READ"]\'),
-                (23, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_custom_id\', \'["BROWSE","READ"]\'),
-                (24, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_created_at\', \'["BROWSE","READ"]\'),
-                (25, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_title\', \'["BROWSE","READ"]\'),
-                (26, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_data\', \'["BROWSE","READ"]\'),
-                (27, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_state\', \'["BROWSE","READ"]\'),
-                (28, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_statuses\', \'["BROWSE","READ"]\'),
-                (29, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_version\', \'["BROWSE","READ"]\'),
-                (30, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status\', \'["BROWSE","READ"]\'),
-                (31, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_uuid\', \'["BROWSE","READ"]\'),
-                (32, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_created_at\', \'["BROWSE","READ"]\'),
-                (33, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_case\', \'["BROWSE","READ"]\'),
-                (34, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_title\', \'["BROWSE","READ"]\'),
-                (35, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_description\', \'["BROWSE","READ"]\'),
-                (36, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_data\', \'["BROWSE","READ"]\'),
-                (37, 3, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_version\', \'["BROWSE","READ"]\'),
-                (38, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case\', \'["BROWSE","READ"]\'),
-                (39, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_uuid\', \'["BROWSE","READ"]\'),
-                (40, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_custom_id\', \'["BROWSE","READ"]\'),
-                (41, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_created_at\', \'["BROWSE","READ"]\'),
-                (42, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_title\', \'["BROWSE","READ"]\'),
-                (43, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_data\', \'["BROWSE","READ"]\'),
-                (44, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_state\', \'["BROWSE","READ"]\'),
-                (45, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_statuses\', \'["BROWSE","READ"]\'),
-                (46, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_version\', \'["BROWSE","READ"]\'),
-                (47, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status\', \'["BROWSE","READ"]\'),
-                (48, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_uuid\', \'["BROWSE","READ"]\'),
-                (49, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_created_at\', \'["BROWSE","READ"]\'),
-                (50, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_case\', \'["BROWSE","READ"]\'),
-                (51, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_title\', \'["BROWSE","READ"]\'),
-                (52, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_description\', \'["BROWSE","READ"]\'),
-                (53, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_data\', \'["BROWSE","READ"]\'),
-                (54, 4, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_version\', \'["BROWSE","READ"]\'),
-                (55, 5, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case\', \'["BROWSE","READ"]\'),
-                (56, 5, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_property\', \'["BROWSE","READ"]\'),
-                (57, 5, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status\', \'["BROWSE","READ"]\'),
-                (58, 5, \'BusinessUnit\', \''.$data['business_unit']['backoffice']['uuid'].'\', \'case_status_property\', \'["BROWSE","READ"]\'),
-                (59, 6, \'BusinessUnit\', NULL, \'entity\', \'["BROWSE","READ","EDIT","ADD","DELETE"]\'),
-                (60, 6, \'BusinessUnit\', NULL, \'property\', \'["BROWSE","READ","EDIT"]\'),
-                (61, 6, \'BusinessUnit\', NULL, \'custom\', \'["BROWSE","READ","EDIT","ADD","DELETE","EXECUTE"]\');
+                (1, 1, \'generic\', NULL, NULL, \'entity\', \'["BROWSE","READ","EDIT","ADD","DELETE"]\'),
+                (2, 1, \'generic\', NULL, NULL, \'property\', \'["BROWSE","READ","EDIT"]\'),
+                (3, 1, \'generic\', NULL, NULL, \'generic\', \'["BROWSE","READ","EDIT","ADD","DELETE","EXECUTE"]\'),
+                (4, 2, \'generic\', NULL, NULL, \'entity\', \'["BROWSE","READ","EDIT","ADD","DELETE"]\'),
+                (5, 2, \'generic\', NULL, NULL, \'property\', \'["BROWSE","READ","EDIT"]\'),
+                (6, 2, \'generic\', NULL, NULL, \'generic\', \'["BROWSE","READ","EDIT","ADD","DELETE","EXECUTE"]\');
         ');
     }
 
@@ -184,10 +126,11 @@ class Version1_0_0 extends AbstractMigration
         $this->addSql('DROP TABLE ds_config');
         $this->addSql('DROP TABLE ds_access');
         $this->addSql('DROP TABLE ds_access_permission');
-        $this->addSql('DROP TABLE ds_session');
         $this->addSql('DROP TABLE app_case');
         $this->addSql('DROP TABLE app_case_status');
         $this->addSql('DROP TABLE app_case_status_trans');
         $this->addSql('DROP TABLE app_case_trans');
+
+        $this->addSql('DROP TABLE ds_session');
     }
 }
